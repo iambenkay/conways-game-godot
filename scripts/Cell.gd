d5737cf130ee9684b43f2dddf4cc4a1dd10951aa
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2022-08-05 10:07:54 +0100

    changes color_from_state() impl

--- scripts/Cell.gd
+++ scripts/Cell.gd
@@ -1,7 +1,5 @@
 extends ColorRect
 
-export var alive = false
-
 var GREEN = Color("1cd02f")
 var TRANSPARENT = Color("00ffffff")
 
@@ -9,6 +7,4 @@ func alive(state):
 	color = color_from_state(state)
 
 func color_from_state(state):
-	if state:
-		return GREEN
-	return TRANSPARENT
+	return GREEN if state else TRANSPARENT
